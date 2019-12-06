02d17c65d61885a88c47e0118fb45c6e0cf8001c
julien <gogrinformatique@gmail.com>
2019-12-06 17:18:54 +0100

    add nex pages

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -21,4 +21,34 @@ class DefaultController extends Controller
         return $this->render('actuality/actuality.twig');
     }
 
+    public function seasonAction (){
+
+        return $this->render('actuality/season.twig');
+    }
+
+    public function nCreationAction (){
+
+        return $this->render('actuality/ncreation.twig');
+    }
+
+    public function pCeationAction (){
+
+        return $this->render('actuality/pcreation.twig');
+    }
+
+    public function exoticAction (){
+
+        return $this->render('actuality/exotic.twig');
+    }
+
+    public function inspirationAction (){
+
+        return $this->render('actuality/inspiration.twig');
+    }
+
+    public function leatherworkAction (){
+
+        return $this->render('actuality/leatherwork.twig');
+    }
+
 }
